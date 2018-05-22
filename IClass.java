4c9d266fe06f9287bf8afcc16b7512e6c223f668
Sai Kalyana Chakravarthi Mogathapalli <smogathapalli@smogathapalli.hyd.corp.google.com>
2018-05-22 16:42:50 +0530

    Initial commit

---
+++ IClass.java
@@ -0,0 +1,8 @@
+package com.example.smogathapalli.instantrun_test_1;
+
+import android.test.InstrumentationTestCase;
+
+public class IClass extends InstrumentationTestCase {
+    public void testEmptyTest(){
+    }
+}
