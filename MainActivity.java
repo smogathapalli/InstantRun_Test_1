ebdee187af8ad83f3511c28205ad8e40b409eeb5
smogathapalli <mskchakravarthi@gmail.com>
2020-08-24 16:16:06 +0530

    Commint 50

--- MainActivity.java
+++ MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         int y = 20;
         int z = 30;
         int i = 40;
+        int j = 50;
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
